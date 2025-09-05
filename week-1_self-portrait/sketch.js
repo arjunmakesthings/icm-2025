97fafb6c05b6ef93452776a72393302a08d4cca6
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-09-05 16:01:15 -0400

    initialise

---
+++ week-1_self-portrait/sketch.js
@@ -0,0 +1,19 @@
+//self-portrait; september 05, 2025.
+
+//use colours from my website: 
+let cols = [
+  [253, 253, 253], //bg
+  [60, 60, 60], //primary
+  [120, 120, 120], //secondary
+  [240, 240, 240], //tertiary
+];
+
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(cols[0]);
+
+  noLoop();
+}
