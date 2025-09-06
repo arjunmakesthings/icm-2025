e7124a015c20cc24f49f5898ca9e85c3cb6ac755
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-09-06 18:24:44 -0400

    make portrait.

--- week-1_self-portrait/sketch.js
+++ week-1_self-portrait/sketch.js
@@ -16,23 +16,64 @@ let world_height = 800;
 let people = [];
 
 //they're all of the same size:
-const size = 4;
+const size = 6;
+
+let photograph; //to use picture later.
+
+//to preload the image, through which people get colour:
+function preload() {
+  photograph = loadImage("/assets/sq_passport-photo.jpg");
+}
 
 function setup() {
   //the world is born:
   createCanvas(world_width, world_height);
 
+  //don't need stroke. 
+  noStroke(); 
+  //get colours from the photograph once:
+  get_colours_from_img(photograph);
+
   //people are born in the world:
-  const space = size * 2; // people give each other some space.
+  const space = size +1; // people give each other some space.
   for (let x = 0 + size; x <= world_width - size; x += space) {
     for (let y = 0 + 4; y <= world_height - 4; y += space) {
+      // //i also want to push the colours of my image here. so:
+      // let col = convert_coordinates_to_colour(x, y, photograph);
+
+      // //check and swap white for black. 
+      // if (col[0]==255 && col[1]==255 && col[2]==255){
+      //   col = [0, 0, 0]; 
+      // }
+
       people.push(new People(x, y));
     }
   }
 }
 
+//helper function to get colours from an image in 'pixels' array.
+function get_colours_from_img(img) {
+  img.resize(0, width); //fit the width of the screen.
+  image(img, 0, 0); //draw from the top-left.
+
+  img.loadPixels();
+  background (cols[0]); 
+}
+
+//helper function to convert coordinates to colours, as passed down in setup.
+function convert_coordinates_to_colour(x, y, img) {
+  let index = (y * img.width + x) * 4;
+
+  let r = img.pixels[index];
+  let g = img.pixels[index + 1];
+  let b = img.pixels[index + 2];
+  let a = img.pixels[index + 3];
+
+  return [r, g, b, a];
+}
+
 function draw() {
-  // background(cols[0]);
+  background(cols[0]);
 
   //each person:
   for (person of people) {
@@ -42,7 +83,7 @@ function draw() {
     person.move();
   }
 
-  //people move to take someone's place, or be in someone's place.
+  //people move to take someone's place, or be in someone's space.
   let moving_person = people[int(random(0, people.length))];
   let static_person = people[int(random(0, people.length))]; //sometimes, the moving and the stationary person are the same. this means you're happy where you are. that's humanely (and probabilistically) rare.
 
@@ -51,7 +92,7 @@ function draw() {
 }
 
 class People {
-  constructor(x, y, col = 0) {
+  constructor(x, y, col = [0, 0, 0, 255]) {
     this.x = x;
     this.y = y;
     this.size = size;
