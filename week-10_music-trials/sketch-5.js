a18df53928e94e88b61c29eacbcec339d0cdad70
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-11-16 18:12:55 -0500

    mic looper.

--- week-10_music-trials/sketch-5.js
+++ week-10_music-trials/sketch-5.js
@@ -1,35 +1,79 @@
 //intent: take mic-input and loop it.
 
-let draw_stuff = true;
-
 let record_button;
-let record_button_text = "record";
 let recording_state = false;
 
-let mic, recorder, recorded;
+let mic, recorder;
+
+let voices = [];
+let currentFile;
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(windowWidth, windowHeight);
+  userStartAudio();
 
-  record_button = createButton(record_button_text);
+  record_button = createButton("record");
   record_button.position(width / 2, height / 2);
+
+  record_button.mousePressed(record_stuff);
+
+  mic = new p5.AudioIn();
+  mic.start();
+
+  recorder = new p5.SoundRecorder();
+  recorder.setInput(mic);
 }
 
 function draw() {
   background(0);
 
-  record_button.mousePressed(() => (recording_state = !recording_state)); 
-
-  if (recording_state == true) {
-
-  } else {
-
-  }
-
-  if (draw_stuff == true) {
+  // draw all recorded voices
+  for (let voice of voices) {
+    voice.display();
   }
 }
 
-function canvasPressed() {
-  userStartAudio();
+function record_stuff() {
+  if (!recording_state) {
+    recording_state = true;
+    record_button.html("stop");
+
+    currentFile = new p5.SoundFile(); 
+    recorder.record(currentFile);
+  } else {
+    recording_state = false;
+    record_button.html("record");
+
+    recorder.stop();
+
+    setTimeout(() => {
+      let v = new Voice(currentFile);
+      v.loop(); //we set it in permanent loop here, and not in draw (otherwise it loops over itself). 
+      voices.push(v);
+    }, 50);
+  }
+}
+
+class Voice {
+  constructor(soundfile) {
+    this.sound= soundfile; 
+
+    this.x = random(width);
+    this.y = random(height);
+    this.color = color(random(255), random(255), random(255));
+  }
+
+  loop() {
+    this.sound.loop(); // start looping this sound
+  }
+
+  stop() {
+    this.sound.stop();
+  }
+
+  display() {
+    fill(this.color);
+    noStroke();
+    ellipse(this.x, this.y, 40, 40); // placeholder visual
+  }
 }
